61dd6b8b1d05bbbb7f89dd4814190d8c11f25bd3
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-09-27 08:07:46 +0200

    Revert "release 0.1.0"

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ Utility cli to anlyse most github statics about projects and users
 #### Docs
 > Comming soon........
 
-
 #### Installation
 
 Check [releases](https://github.com/veritem/gts/releases) add download a binary for your platform
--- src/client.rs
+++ src/client.rs
@@ -1,57 +1,55 @@
 const API_ACCEPT: &'static str = "application/vnd.github.v3+json";
 const API_USER_AGENT: &'static str =
     concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
-const GITHUB_BASE_URL: &'static str = "https://api.github.com";
 
-use crate::auth::Auth;
-use reqwest::Url;
-// use serde::de::DeserializeOwned;
+use serde::de::DeserializeOwned;
 
-#[derive(Debug)]
+// use crate::auth::Auth;
+
 pub struct Client {
-    client: reqwest::Client,
-    pub base_url: String,
-    auth: Auth,
+    // headers: String,
+    base_url: String,
+    //auth: Auth,
 }
 
 impl Default for Client {
     fn default() -> Self {
         Self {
-            base_url: Url::parse(GITHUB_BASE_URL).unwrap().to_string(),
-            client: reqwest::ClientBuilder::new()
-                .user_agent(API_USER_AGENT)
-                .build()
-                .unwrap(),
-            auth: Auth::default(),
+            base_url: String::from("https://api.github.com/"),
+            //  auth: Auth::default(),
         }
     }
 }
 
 impl Client {
-    pub fn add_auth(&mut self, token: String) -> &Self {
-        self.auth = Auth::PersonalToken(token);
-        self
+    pub fn new() -> Self {
+        Self::default()
     }
 
-    pub fn build(self) -> Result<reqwest::Client, reqwest::Error> {
-        let mut headers = reqwest::header::HeaderMap::new();
+    // pub fn add_header(mut &self, header: &str) -> Self {}
+    // pub fn add_auth(mut self, token: &str) -> Self {
+    //     self.auth = Auth::PersonalToken;
+    //     self
+    // }
 
-        headers.append(
-            reqwest::header::ACCEPT,
-            format!("{}", API_ACCEPT).parse().unwrap(),
-        );
+    pub async fn get<T>(&self, url: &str) -> Result<T, reqwest::Error>
+    where
+        T: DeserializeOwned,
+    {
+        let resp = reqwest::Client::new()
+            .get(format!("{}{}", &self.base_url, url))
+            .header("Accept", API_ACCEPT)
+            .header("User-Agent", API_USER_AGENT)
+            .send()
+            .await;
 
-        if let Auth::PersonalToken(token) = self.auth {
-            headers.append(
-                reqwest::header::AUTHORIZATION,
-                format!("Bearer {}", token).parse().unwrap(),
-            );
+        match resp {
+            Ok(success) => {
+                let resp_text = success.text().await?;
+                let text_match = serde_json::from_str(&resp_text).unwrap();
+                Ok(text_match)
+            }
+            Err(e) => Err(e),
         }
-        let client = reqwest::Client::builder()
-            .user_agent("gts")
-            .default_headers(headers)
-            .build()?;
-
-        Ok(client)
     }
 }
